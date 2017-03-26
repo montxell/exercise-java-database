6712e9e96c759658b85566114746544f60c95d62
Montse Ariso <montxelltech@gmail.com>
2017-03-27 01:36:15 +0200

    Add close method to class DatabaseUtil

--- src/main/java/com/codethen/javadbexercise/DatabaseUtil.java
+++ src/main/java/com/codethen/javadbexercise/DatabaseUtil.java
@@ -1,8 +1,6 @@
 package com.codethen.javadbexercise;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseUtil {
 
@@ -11,13 +9,30 @@ public class DatabaseUtil {
         String host = "localhost";
         String schema = "test";
         String user = "root";
-        String pwd = "monsql";
+        String pwd = "moncode83";
 
         Connection conn = DriverManager.getConnection(
                 "jdbc:mysql://" + host + "/" + schema + "?user=" + user + "&password=" + pwd);
 
 
         return conn;
+    }
+
+
+    public static void close(ResultSet rs, PreparedStatement stmt, Connection conn) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // will print the exception stack trace
+        }
     }
 
 }
