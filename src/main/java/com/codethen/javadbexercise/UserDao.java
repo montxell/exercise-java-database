905f1e36de391fb9c8b53a5426c52dc8212769a2
Montse Ariso <montxelltech@gmail.com>
2017-04-03 01:50:09 +0200

    Rename class UserDao to UserDaoComplex and add interface UserDao

--- src/main/java/com/codethen/javadbexercise/UserDao.java
+++ src/main/java/com/codethen/javadbexercise/UserDao.java
@@ -1,183 +1,23 @@
 package com.codethen.javadbexercise;
 
-
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
-/** Responsible of talking with the database to read/write users */
-public class UserDao {
+public interface UserDao {
 
+    // Especifica los métodos del DaoComplex que puede llamar pero sin implementar.
+    // Todos los métodos del interface son public y abstract.
+    // Abstract porque están sin implementar.
 
-    public List<User> findAll() {
+    // Lo mejor es que las clases estén aisladas unas y otras, y si se tienen que relacionar o conocer se deben hacer
+    // interfaces.
 
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
 
-        try {
+    /** finds all users */
+    List<User> findAll();
 
-            // get connection
-            conn = DatabaseUtil.getConnection();
-
-            // prepare and execute query
-            stmt = conn.prepareStatement("select * from users");
-            rs = stmt.executeQuery();
-
-            List<User> users = new ArrayList<User>();
-
-            while (rs.next()) {
-
-                User user = getUser(rs);
-                users.add(user);
-
-            }
-
-            return users;
-
-        } catch(Exception e) {
-            throw new RuntimeException("error finding users", e);
-        } finally {
-            DatabaseUtil.close(rs, stmt, conn);
-        }
-    }
-
-
-
-    public User findById(int id) {
-
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
-
-
-        try {
-
-            final User result;
-
-            conn = DatabaseUtil.getConnection();
-
-            stmt = conn.prepareStatement("select * from users where id = ?");
-            stmt.setInt(1, id);
-            rs = stmt.executeQuery();
-
-
-            if (rs.next()) {
-
-                User user = getUser(rs);
-
-                result = user;
-
-            } else {
-
-                result = null;
-            }
-
-            return result;
-
-
-        // boilerplate (exception handling and closing resources)
-        } catch(Exception e) {
-            throw new RuntimeException("error finding user", e);
-        } finally {
-            DatabaseUtil.close(rs, stmt, conn);
-        }
-    }
-
-
-
-    public void create(User user) {
-
-        Connection conn = null;
-        PreparedStatement stmt = null;
-
-        try {
-
-            conn = DatabaseUtil.getConnection();
-
-            // prepare and execute update
-            stmt = conn.prepareStatement("insert into users (username, name, email) values (?, ?, ?)");
-            stmt.setString(1, user.getUsername());
-            stmt.setString(2, user.getName());
-            stmt.setString(3, user.getEmail());
-
-            stmt.executeUpdate();
-
-
-        } catch(Exception e) {
-            throw new RuntimeException("error creating user", e);
-        } finally {
-            DatabaseUtil.close(null, stmt, conn);
-        }
-    }
-
-
-
-    public void update(User user) {
-
-        Connection conn = null;
-        PreparedStatement stmt = null;
-
-        try {
-
-            conn = DatabaseUtil.getConnection();
-
-            stmt = conn.prepareStatement("update users set username = ?, name = ?, email = ? where id = ?");
-            stmt.setString(1, user.getUsername());
-            stmt.setString(2, user.getName());
-            stmt.setString(3, user.getEmail());
-            stmt.setInt(4, user.getId());
-
-            stmt.executeUpdate();
-
-
-        } catch(Exception e) {
-            throw new RuntimeException("error updating user", e);
-        } finally {
-            DatabaseUtil.close(null, stmt, conn);
-        }
-    }
-
-
-
-    public void delete(int id) {
-
-        Connection conn = null;
-        PreparedStatement stmt = null;
-
-        try {
-
-            conn = DatabaseUtil.getConnection();
-
-            stmt = conn.prepareStatement("delete from users where id = ?");
-            stmt.setInt(1, id);
-
-            stmt.executeUpdate();
-
-
-        } catch(Exception e) {
-            throw new RuntimeException("error deleting user", e);
-        } finally {
-            DatabaseUtil.close(null, stmt, conn);
-        }
-    }
-
-
-
-    private User getUser(ResultSet rs) throws SQLException {
-
-        int id = rs.getInt("id");
-        String username = rs.getString("username");
-        String name = rs.getString("name");
-        String email = rs.getString("email");
-
-        User user = new User();
-        user.setId(id);
-        user.setUsername(username);
-        user.setName(name);
-        user.setEmail(email);
-
-        return user;
-    }
+    User findById(int id);
+    void create(User user);
+    void update(User user);
+    void delete(int id);
 
 }
---
+++ src/main/java/com/codethen/javadbexercise/UserDaoComplex.java
@@ -0,0 +1,186 @@
+package com.codethen.javadbexercise;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/** Responsible of talking with the database to read/write users */
+public class UserDaoComplex implements UserDao {
+
+    // Si se implementa el interface se tiene que poner el Override. Si algún metodo no está en el interface el
+    // override lo indica que está mal
+
+    @Override
+    public List<User> findAll() {
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+
+            // get connection
+            conn = DatabaseUtil.getConnection();
+
+            // prepare and execute query
+            stmt = conn.prepareStatement("select * from users");
+            rs = stmt.executeQuery();
+
+            List<User> users = new ArrayList<User>();
+
+            while (rs.next()) {
+
+                User user = getUser(rs);
+                users.add(user);
+
+            }
+
+            return users;
+
+        } catch(Exception e) {
+            throw new RuntimeException("error finding users", e);
+        } finally {
+            DatabaseUtil.close(rs, stmt, conn);
+        }
+    }
+
+
+    @Override
+    public User findById(int id) {
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+
+        try {
+
+            final User result;
+
+            conn = DatabaseUtil.getConnection();
+
+            stmt = conn.prepareStatement("select * from users where id = ?");
+            stmt.setInt(1, id);
+            rs = stmt.executeQuery();
+
+
+            if (rs.next()) {
+
+                User user = getUser(rs);
+
+                result = user;
+
+            } else {
+
+                result = null;
+            }
+
+            return result;
+
+
+        // boilerplate (exception handling and closing resources)
+        } catch(Exception e) {
+            throw new RuntimeException("error finding user", e);
+        } finally {
+            DatabaseUtil.close(rs, stmt, conn);
+        }
+    }
+
+
+    @Override
+    public void create(User user) {
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+
+            conn = DatabaseUtil.getConnection();
+
+            // prepare and execute update
+            stmt = conn.prepareStatement("insert into users (username, name, email) values (?, ?, ?)");
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getName());
+            stmt.setString(3, user.getEmail());
+
+            stmt.executeUpdate();
+
+
+        } catch(Exception e) {
+            throw new RuntimeException("error creating user", e);
+        } finally {
+            DatabaseUtil.close(null, stmt, conn);
+        }
+    }
+
+
+    @Override
+    public void update(User user) {
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+
+            conn = DatabaseUtil.getConnection();
+
+            stmt = conn.prepareStatement("update users set username = ?, name = ?, email = ? where id = ?");
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getName());
+            stmt.setString(3, user.getEmail());
+            stmt.setInt(4, user.getId());
+
+            stmt.executeUpdate();
+
+
+        } catch(Exception e) {
+            throw new RuntimeException("error updating user", e);
+        } finally {
+            DatabaseUtil.close(null, stmt, conn);
+        }
+    }
+
+
+    @Override
+    public void delete(int id) {
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try {
+
+            conn = DatabaseUtil.getConnection();
+
+            stmt = conn.prepareStatement("delete from users where id = ?");
+            stmt.setInt(1, id);
+
+            stmt.executeUpdate();
+
+
+        } catch(Exception e) {
+            throw new RuntimeException("error deleting user", e);
+        } finally {
+            DatabaseUtil.close(null, stmt, conn);
+        }
+    }
+
+
+
+    private User getUser(ResultSet rs) throws SQLException {
+
+        int id = rs.getInt("id");
+        String username = rs.getString("username");
+        String name = rs.getString("name");
+        String email = rs.getString("email");
+
+        User user = new User();
+        user.setId(id);
+        user.setUsername(username);
+        user.setName(name);
+        user.setEmail(email);
+
+        return user;
+    }
+
+}
