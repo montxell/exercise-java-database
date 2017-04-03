e1e030682a5048cce41ac65405f4bb5755dce8b2
Montse Ariso <montxelltech@gmail.com>
2017-04-03 02:57:21 +0200

    Add class UserDaoSpring and DataSource to class MainTestDao

--- src/main/java/com/codethen/javadbexercise/MainTestDao.java
+++ src/main/java/com/codethen/javadbexercise/MainTestDao.java
@@ -1,12 +1,23 @@
 package com.codethen.javadbexercise;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
 import java.util.List;
 
 public class MainTestDao {
 
     public static void main(String[] args) {
 
-        UserDao userDao = new UserDao();
+        //UserDaoComplex userDao = new UserDaoComplex();
+
+        // Se configura el DataSource del tipo de Mysql, pero después se aplica en UserDaoSpring y éste no tiene por qué saber que tipo de DataSource
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL("jdbc:mysql://localhost/test");  // servidor: localhost   schema: test
+        dataSource.setUser("root");
+        dataSource.setPassword("moncode83");
+
+
+        UserDao userDao = new UserDaoSpring(dataSource);
 
 
         User user = userDao.findById(2);
